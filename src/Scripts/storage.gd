df825449389fc6278f6fec72840978c9e91f1662
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2026-07-25 22:59:31 +0530

    feat: Implement day progression and game over conditions

--- src/Scripts/storage.gd
+++ src/Scripts/storage.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var base_storage_capacity: int = 5
 @export var storage_capacity_per_tier: int = 5
 
-var lodge_tier: int = 1
+var lodge_tier: int = 0
 
 
 func _ready() -> void:
